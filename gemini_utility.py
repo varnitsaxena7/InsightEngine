7e48b13452d81282588fe0493e58b33eaf7757e7
varnitsaxena7 <118539746+varnitsaxena7@users.noreply.github.com>
2024-03-24 15:19:27 +0530

    I have added the whole code for Insight Engine

---
+++ gemini_utility.py
@@ -0,0 +1,83 @@
+import os
+import json
+from PIL import Image
+import google.generativeai as genai
+
+working_dir = os.path.dirname(os.path.abspath(__file__))
+
+config_file_path = f"{working_dir}/config.json"
+config_data = json.load(open("config.json"))
+
+GOOGLE_API_KEY = config_data["GOOGLE_API_KEY"]
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+def load_gemini_pro_model():
+    gemini_pro_model = genai.GenerativeModel("gemini-pro")
+    return gemini_pro_model
+
+def gemini_pro_vision_response(prompt, image):
+    gemini_pro_vision_model = genai.GenerativeModel("gemini-pro-vision")
+    response = gemini_pro_vision_model.generate_content([prompt, image])
+    result = response.text
+    return result
+
+def embeddings_model_response(input_text):
+    embedding_model = "models/embedding-001"
+    embedding = genai.embed_content(model=embedding_model,
+                                    content=input_text,
+                                    task_type="retrieval_document")
+    embedding_list = embedding["embedding"]
+    return embedding_list
+
+def gemini_pro_response(user_prompt):
+    gemini_pro_model = genai.GenerativeModel("gemini-pro")
+    response = gemini_pro_model.generate_content(user_prompt)
+    result = response.text
+    return result
+
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+nltk.download('vader_lexicon')
+sid = SentimentIntensityAnalyzer()
+def sentiment_analysis(text):
+    sentiment_scores = sid.polarity_scores(text)
+    return sentiment_scores
+
+import speech_recognition as sr
+import streamlit as st
+def recognize_speech():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        st.write("Listening...")
+        recognizer.adjust_for_ambient_noise(source, duration=1)
+        audio_data = recognizer.listen(source)
+    try:
+        st.write("Processing...")
+        text = recognizer.recognize_google(audio_data)
+        return text
+    except sr.UnknownValueError:
+        st.write("Sorry, I could not understand what you said.")
+    except sr.RequestError:
+        st.write("Sorry, the service is unavailable.")
+
+
+import streamlit as st
+from youtube_transcript_api import YouTubeTranscriptApi
+import base64
+
+def transcribe_video(video_url, language='en', paragraph_length=300):
+    try:
+        video_id = video_url.split('v=')[-1]
+        transcript_list = YouTubeTranscriptApi.get_transcript(video_id, languages=[language])
+        transcript = ' '.join(line['text'] for line in transcript_list)
+        paragraphs = [transcript[i:i+paragraph_length] for i in range(0, len(transcript), paragraph_length)]
+        return paragraphs, transcript
+    except Exception as e:
+        st.error(f"Error: {e}")
+        return None, None
+
+def display_paragraphs(paragraphs):
+    for i, paragraph in enumerate(paragraphs):
+        st.markdown(f"**Paragraph {i+1}:**")
+        st.write(paragraph)
---
+++ main.py
@@ -0,0 +1,381 @@
+import os
+from PIL import Image
+import streamlit as st
+import time
+import json
+import base64
+from gemini_utility import (load_gemini_pro_model, gemini_pro_response, gemini_pro_vision_response, embeddings_model_response)
+
+def translate_role_for_streamlit(user_role):
+    if user_role == "model":
+        return "assistant"
+    else:
+        return user_role
+
+st.set_page_config(
+    page_title="InsightEngine",
+    page_icon="💡",
+    layout="centered",  
+)
+
+# Features dropdown menu
+st.title("InsightEngine 💡")
+selected = st.sidebar.selectbox(
+    'Select Feature',
+    ['ChatBot', 'Image Captioning', 'Embed text', 'Ask me anything','Sentiment Analysis','Language Translation','Topic Modeling','Youtube Transcriber','Document Summarizer'],
+    index=0
+
+)
+chat_history = []
+def clear_chat():
+            global chat_history
+            chat_history.clear()
+            
+# chatbot page
+if selected == 'ChatBot':
+        model = load_gemini_pro_model()
+
+# Initialize chat session in Streamlit if not already present
+        if "chat_session" not in st.session_state:
+            st.session_state.chat_session = model.start_chat(history=[])
+
+        # Display the chatbot's title on the page
+        st.title("🤖 TalkBuddy")
+
+        # Input field for user's message
+        user_prompt = st.text_input("Ask InsightEngine...")
+
+        # Button to generate a response
+        if st.button("Generate Response"):
+            if user_prompt:
+                # Add user's message to chat and display it
+                st.write("You: " + user_prompt)
+
+                # Send user's message to Gemini-Pro and get the response
+                gemini_response = st.session_state.chat_session.send_message(user_prompt)
+
+                # Display Gemini-Pro's response
+                st.write("Assistant: " + gemini_response.text)
+                st.session_state.chat_session.history.append(gemini_response)
+
+        # Option to clear chat history
+        if st.button("Clear Chat History"):
+                clear_chat()
+                st.write("Chat history cleared.")
+
+# Image captioning page
+if selected == "Image Captioning":
+    st.title("📷 Image Teller")
+
+    uploaded_image = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
+
+    if uploaded_image is not None:
+            # Display uploaded image
+            st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
+
+            # Text input for caption prompt
+            caption_prompt = st.text_input("Caption Prompt", "Write a short caption for this image")
+
+            # Button to generate caption
+            if st.button("Generate Caption"):
+                with st.spinner('Generating Caption...'):
+                    # Open uploaded image
+                    image = Image.open(uploaded_image)
+
+                    # Display image and caption in columns
+                    col1, col2 = st.columns([3, 1])
+                    with col1:
+                        resized_img = image.resize((800, 500))
+                        st.image(resized_img, caption="Original Image", use_column_width=True)
+
+                    with col2:
+                        # Generate caption using Gemini-Pro
+                        caption = gemini_pro_vision_response(caption_prompt, image)
+                        st.info(caption)
+
+
+# Text embedding model
+if selected == "Embed text":
+    st.title("🔡 Embed Text")
+
+    user_prompt = st.text_area(label='', placeholder="Enter the text to get embeddings")
+
+    if st.button("Get Response"):
+        with st.spinner('Generating Embeddings...'):
+            response = embeddings_model_response(user_prompt)
+            st.markdown(response)
+
+
+# Ask me anything page
+if selected == "Ask me anything":
+    st.title("❓ Ask me a question")
+
+    user_prompt = st.text_area(label='', placeholder="Ask me anything...")
+
+    if st.button("Get Response"):
+        with st.spinner('Thinking...'):
+            response = gemini_pro_response(user_prompt)
+            st.markdown(response)
+
+
+
+from gemini_utility import sentiment_analysis
+import random
+# Inside the if-else block for the selected feature
+if selected == 'Sentiment Analysis':  # Add this option in the dropdown menu
+    st.title("💬 Mood Mapper")
+    def get_mood_phrases():
+        with open('mood_phrases.json') as file:
+            mood_phrases = json.load(file)
+            return mood_phrases
+
+# Function to generate mood-based suggestions
+    def get_suggestions(sentiment):
+         mood_phrases = get_mood_phrases()
+         return random.choice(mood_phrases[sentiment])
+
+    # Text input for user's message
+    user_text = st.text_area(label='', placeholder="Enter the text for sentiment analysis")
+
+    # Button to trigger sentiment analysis
+    if st.button("Analyze Sentiment"):
+        with st.spinner('Analyzing Sentiment...'):
+            # Perform sentiment analysis
+            sentiment_scores = sentiment_analysis(user_text)
+
+            # Display sentiment analysis results
+            p=sentiment_scores['pos']
+            n=sentiment_scores['neg']
+            ne=sentiment_scores['neu']
+            c=sentiment_scores['compound']
+            st.write("Sentiment Analysis Results:")
+            if(c>0):
+                 sentiment = 'positive'
+                 st.write("Positive 😄")
+            elif(c<0):
+                 sentiment = 'negative'
+                 st.write("Negative 😔")
+            else:
+                sentiment = 'neutral'
+                st.write("Neutral 😐")
+            st.subheader("Sentiment Distribution:")
+            st.write("Positive: {}%".format(round(p * 100, 2)))
+            st.write("Negative: {}%".format(round(n * 100, 2)))
+            st.write("Neutral: {}%".format(round(ne * 100, 2)))
+            st.subheader("Suggestions Based on Your Mood:")
+            suggestions = get_suggestions(sentiment)
+            st.write(suggestions)
+
+
+from gemini_utility import recognize_speech
+from googletrans import Translator
+import base64
+if selected == 'Voice Interaction':
+    def download_file(content, filename):
+                b64 = base64.b64encode(content.encode()).decode()
+                href = f'<a href="data:file/txt;base64,{b64}" download="{filename}">Download {filename}</a>'
+                return href
+    st.title("🎙️ VoiceBuddy")
+    st.write("Press the button and speak to interact.")
+    if "cost" not in st.session_state:
+        st.session_state.cost = ""
+    if st.button("Start Recording"):
+        # Record voice input
+        user_voice_input = recognize_speech()
+        st.write("You said:", user_voice_input)
+        if user_voice_input:
+            st.session_state.cost += "\n" + user_voice_input
+    with st.form("Result"):
+        result = st.text_area("Transcribed Text", value=st.session_state.cost )
+        st.markdown(download_file(st.session_state.cost, "transcribe.txt"), unsafe_allow_html=True)
+        
+        submit_button = st.form_submit_button("Continue by clicking Start Recording")
+        if submit_button:
+           st.session_state.cost += " " + recognize_speech() 
+
+
+
+if selected =='Language Translation':
+                import streamlit as st
+                from googletrans import Translator
+
+                # Initialize the translator
+                translator = Translator()
+
+                # Streamlit app title and description
+                st.title("Linguify 🗣️")
+                st.write("Translate text from one language to another using Google Translate.")
+
+                # Input field for user's text
+                user_text = st.text_area("Enter text to translate")
+
+                # List of supported languages
+                languages = {
+                    "Afrikaans": "af", "Albanian": "sq", "Arabic": "ar", "Armenian": "hy", "Azerbaijani": "az", 
+                    "Basque": "eu", "Belarusian": "be", "Bengali": "bn", "Bosnian": "bs", "Bulgarian": "bg", 
+                    "Catalan": "ca", "Cebuano": "ceb", "Chinese (Simplified)": "zh-CN", "Chinese (Traditional)": "zh-TW", 
+                    "Corsican": "co", "Croatian": "hr", "Czech": "cs", "Danish": "da", "Dutch": "nl", "English": "en", 
+                    "Esperanto": "eo", "Estonian": "et", "Filipino": "tl", "Finnish": "fi", "French": "fr", "Frisian": "fy", 
+                    "Galician": "gl", "Georgian": "ka", "German": "de", "Greek": "el", "Gujarati": "gu", "Haitian Creole": "ht", 
+                    "Hausa": "ha", "Hawaiian": "haw", "Hebrew": "iw", "Hindi": "hi", "Hmong": "hmn", "Hungarian": "hu", 
+                    "Icelandic": "is", "Igbo": "ig", "Indonesian": "id", "Irish": "ga", "Italian": "it", "Japanese": "ja", 
+                    "Javanese": "jw", "Kannada": "kn", "Kazakh": "kk", "Khmer": "km", "Korean": "ko", "Kurdish": "ku", 
+                    "Kyrgyz": "ky", "Lao": "lo", "Latin": "la", "Latvian": "lv", "Lithuanian": "lt", "Luxembourgish": "lb", 
+                    "Macedonian": "mk", "Malagasy": "mg", "Malay": "ms", "Malayalam": "ml", "Maltese": "mt", "Maori": "mi", 
+                    "Marathi": "mr", "Mongolian": "mn", "Myanmar (Burmese)": "my", "Nepali": "ne", "Norwegian": "no", 
+                    "Nyanja (Chichewa)": "ny", "Pashto": "ps", "Persian": "fa", "Polish": "pl", "Portuguese": "pt", 
+                    "Punjabi": "pa", "Romanian": "ro", "Russian": "ru", "Samoan": "sm", "Scots Gaelic": "gd", 
+                    "Serbian": "sr", "Sesotho": "st", "Shona": "sn", "Sindhi": "sd", "Sinhala (Sinhalese)": "si", 
+                    "Slovak": "sk", "Slovenian": "sl", "Somali": "so", "Spanish": "es", "Sundanese": "su", "Swahili": "sw", 
+                    "Swedish": "sv", "Tagalog (Filipino)": "tl", "Tajik": "tg", "Tamil": "ta", "Telugu": "te", "Thai": "th", 
+                    "Turkish": "tr", "Ukrainian": "uk", "Urdu": "ur", "Uzbek": "uz", "Vietnamese": "vi", "Welsh": "cy", 
+                    "Xhosa": "xh", "Yiddish": "yi", "Yoruba": "yo", "Zulu": "zu"
+                }
+
+                # Convert language dictionary to list for selectbox
+                language_options = list(languages.keys())
+
+                # Input field for choosing the source language
+                source_language = st.selectbox("Select source language", language_options)
+
+                # Input field for choosing the target language
+                target_language = st.selectbox("Select target language", language_options)
+
+                # Button to trigger translation
+                if st.button("Translate"):
+                    with st.spinner('Translating...'):
+                        # Perform translation
+                        translation = translator.translate(user_text, src=languages[source_language], dest=languages[target_language])
+                        translated_text = translation.text
+
+                        # Display translated text
+                        st.write("Translated Text:")
+                        st.write(translated_text)
+
+if selected=='Topic Modeling':
+        import streamlit as st
+        from sklearn.feature_extraction.text import CountVectorizer
+        from sklearn.decomposition import LatentDirichletAllocation
+        import numpy as np
+        import pandas as pd
+
+        def perform_topic_modeling(documents, num_topics=5, max_df=0.95, min_df=2, stop_words='english'):
+            vectorizer = CountVectorizer(max_df=max_df, min_df=min_df, stop_words=stop_words)
+            dtm = vectorizer.fit_transform(documents)
+
+            lda_model = LatentDirichletAllocation(n_components=num_topics, random_state=42)
+            lda_model.fit(dtm)
+
+            return lda_model, vectorizer
+
+        def display_topics(model, vectorizer, n_top_words=10):
+            feature_names = np.array(vectorizer.get_feature_names_out())
+            topics = []
+            for topic_idx, topic in enumerate(model.components_):
+                top_words_idx = topic.argsort()[:-n_top_words - 1:-1]
+                top_words = feature_names[top_words_idx]
+                topics.append(top_words)
+            return topics
+
+        st.title("Topic Modeling 📊")
+        st.write("Explore the top words for each topic to gain insights and understand the key themes within your text data.")
+        st.write("Customization Options")
+        num_topics = st.number_input("Number of Topics", min_value=1, max_value=10, value=5, step=1)
+        max_df = st.slider("Max Document Frequency", min_value=0.1, max_value=1.0, value=0.95, step=0.05)
+        min_df = st.number_input("Min Document Frequency", min_value=1, max_value=10, value=2, step=1)
+        stop_words = "english"
+
+        option = st.radio("Select Input Option", ("Upload Documents", "Write Text"))
+
+        if option == "Upload Documents":
+            uploaded_file = st.file_uploader("Upload your documents", type=["txt", "csv"])
+            if uploaded_file is not None:
+                
+                if uploaded_file.type == "txt":
+                    documents = uploaded_file.read().decode("utf-8").splitlines()
+                elif uploaded_file.type == "csv":
+                    df = pd.read_csv(uploaded_file)
+                    documents = df[df.columns[0]].astype(str).tolist()
+                
+                if len(documents) > 0:
+                    lda_model, vectorizer = perform_topic_modeling(documents, num_topics=num_topics, max_df=max_df, min_df=min_df, stop_words=stop_words)
+                    topics = display_topics(lda_model, vectorizer)
+                    st.subheader("Top Words for Each Topic")
+                    for idx, topic_words in enumerate(topics):
+                        st.write(f"Topic {idx + 1}: {', '.join(topic_words)}")
+                else:
+                    st.warning("No documents found in the uploaded file.")
+            else:
+                st.warning("Please upload a text file to perform topic modeling.")
+        else:
+            text_input = st.text_area("Enter your text here", height=200)
+            if st.button("Perform Topic Modeling"):
+                if text_input:
+                    documents = text_input.split("\n")
+                    lda_model, vectorizer = perform_topic_modeling(documents, num_topics=num_topics, max_df=max_df, min_df=min_df, stop_words=stop_words)
+                    topics = display_topics(lda_model, vectorizer)
+                    st.subheader("Top Words for Each Topic")
+                    for idx, topic_words in enumerate(topics):
+                        st.write(f"Topic {idx + 1}: {', '.join(topic_words)}")
+                else:
+                    st.warning("Please enter some text to perform topic modeling.")
+
+if selected=="Youtube Transcriber":
+            from gemini_utility import transcribe_video,display_paragraphs
+            st.title("YouTube Video Transcriber")
+
+            video_url = st.text_input("Enter YouTube video URL:")
+            language = st.radio("Select Language:", options=['English', 'Hindi'])
+            paragraph_length = st.slider("Paragraph Length", min_value=50, max_value=1000, step=50, value=300)
+
+            if st.button("Transcribe"):
+                if video_url:
+                    st.write("Transcribing...")
+                    if language == 'English':
+                        paragraphs, transcript = transcribe_video(video_url, 'en', paragraph_length)
+                    elif language == 'Hindi':
+                        paragraphs, transcript = transcribe_video(video_url, 'hi', paragraph_length)
+                    if paragraphs:
+                        st.header("Transcript:")
+                        display_paragraphs(paragraphs)
+                        
+                        if transcript:
+                            b64_text = base64.b64encode(transcript.encode()).decode()
+                            href = f'<a href="data:file/txt;base64,{b64_text}" download="Transcript.txt">Download Transcript File</a>'
+                            st.markdown(href, unsafe_allow_html=True)
+                        else:
+                            st.warning("No transcript available to download.")
+                else:
+                    st.warning("Please enter a YouTube video URL.")
+                    
+if selected=="Document Summarizer":
+     st.title("Document Summarizer 📄")
+     st.write("Summarize your document quickly and efficiently!")
+     import spacy
+        # Text input for user to enter document
+     document = st.text_area("Enter your document here")
+
+        # Button to trigger summarization
+     if st.button("Summarize"):
+            # Process the document using spaCy
+            nlp = spacy.load("en_core_web_sm")
+            doc = nlp(document)
+
+            # Calculate sentence importance scores based on TextRank algorithm
+            # Summarization by extracting the most important sentences
+            sentence_scores = {}
+            for sent in doc.sents:
+                sentence_scores[sent] = sum([token.rank for token in sent if not token.is_stop])
+
+            # Sort sentences by importance score in descending order
+            sorted_sentences = sorted(sentence_scores, key=sentence_scores.get, reverse=True)
+
+            # Number of sentences in the summarized version
+            num_sentences_summary = min(len(sorted_sentences), 5)  # Set the number of sentences in the summary
+
+            # Generate the summary
+            summary = " ".join([str(sent) for sent in sorted_sentences[:num_sentences_summary]])
+
+            # Display summarized text
+            st.subheader("Summary:")
+            st.write(summary)
