0c17c0aa5a49a6a7d734545a41f66fbe4143b28e
varnitsaxena7 <118539746+varnitsaxena7@users.noreply.github.com>
2025-12-26 13:57:56 +0530

    Update gemini_utility.py

--- gemini_utility.py
+++ gemini_utility.py
@@ -8,10 +8,6 @@ working_dir = os.path.dirname(os.path.abspath(__file__))
 config_file_path = f"{working_dir}/config.json"
 config_data = json.load(open("config.json"))
 
-GOOGLE_API_KEY = config_data["GOOGLE_API_KEY"]
-
-genai.configure(api_key=GOOGLE_API_KEY)
-
 def load_gemini_pro_model():
     gemini_pro_model = genai.GenerativeModel("gemini-1.5-flash")
     return gemini_pro_model
